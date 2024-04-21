0acd9f0f7089ba59dc2ea3cf51b11ae2a2288c90
hemanggs <hemangmanhas@gmail.com>
2024-04-21 10:52:44 +0530

    Minor Fronted Edits Pending

---
+++ README.md
@@ -0,0 +1,8 @@
+#LipSyncer
+
+Slient Speech Recognition : Automatic Lip reading Model using 3D CNN and GRU
+
+Link to Demo Video:
+
+Link to Project report:
+
---
+++ app/pages/8-Developer_Profiles.py
@@ -0,0 +1,24 @@
+import streamlit as st
+st.set_page_config(layout="wide")
+
+st.markdown(
+        """
+        <style>
+            [data-testid="stSidebarNav"] {
+                
+                background-image: url("https://www.dropbox.com/scl/fi/1diny80z7lfy9xnif7wa7/Screenshot_2023-08-30_at_11.47.40_PM.png?rlkey=30biilu3jrlpdjlird0itaco9&dl=0&raw=1");
+                background-repeat: no-repeat;
+                padding-top: 20px;
+                background-size: 290px 80px;
+                background-position: 20px 20px;
+            }
+            
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+st.header('Team Members',divider="blue")
+st.title('Hemang Manhas')
+st.title('Nisha Dhaka')
+st.title('Riya')
+
